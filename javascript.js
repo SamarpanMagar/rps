64a1e9a83a94a5efec9f92476cdf32eeb63208d3
Samarpan Magar <samarpanmagar53@gmail.com>
2023-12-04 10:54:50 +0545

    Add UI Functionality from js

--- javascript.js
+++ javascript.js
@@ -1,19 +1,27 @@
+// Button selector and quiries
+let buttons = document.querySelectorAll('button');
+for (button of buttons) {
+    button.addEventListener("click", game);
+}
+
 // Initialize game
 let playerScore = 0;
 let computerScore = 0;
+let roundsPlayed = 0;
 
 // Game start
-function game () {
-    let playerChoice = getPlayerChoice();
+function game (event) {
+    let playerChoice = getPlayerChoice(event)
     let computerChoice = getComputerChoice();
     let result = compareChoices(playerChoice, computerChoice);
     updateScore(result);
     getGameStatus(playerChoice, computerChoice, result);
+    getGameTotalStatus();
 }
 
 // Get player choice
-function getPlayerChoice () {
-    return (prompt("Play a choice: ").trim()).toLowerCase();
+function getPlayerChoice (event) {
+return event.target.textContent.toLowerCase();
 }
 
 // Get computer choice
@@ -66,29 +74,51 @@ function updateScore (result) {
     }
 }
 
+// Text DOM selector and queries
+let movesPlayed = document.querySelector('.movesPlayed');
+let gameResult = document.querySelector('.gameResult');
+let gameScore = document.querySelector('.gameScore');
+let gameTotalResult = document.querySelector('.gameTotalResult');
+
 // Get game status and scores
 function getGameStatus (playerChoice, computerChoice, result) {
-    console.log(`You played ${playerChoice} and computer played ${computerChoice}.`);
+    movesPlayed.textContent = (`You played ${playerChoice} and computer played ${computerChoice}.`);
     if (result == 'tie') {
-        console.log('It was a tie.');
+        gameResult.textContent = ('It was a tie.');
     }
     else {
-        console.log(`Round won by ${result}`);
+        gameResult.textContent = (`Round won by ${result}`);
     }
-    console.log(`Player score: ${playerScore} --- Computer score: ${computerScore}
+        gameScore.textContent = (`Player score: ${playerScore} --- Computer score: ${computerScore}
     `);
+}
+
+// Check if rounds played is 5 
+function getGameTotalStatus () {
+    roundsPlayed++;
+    if (roundsPlayed >= 5) {
+        getGameWinner();
+    }
 }
 
 // Get game winner
 function getGameWinner () {
     if (playerScore == computerScore) {
-        console.log(`It was a tie, can you believe it?`);
+        gameTotalResult.textContent = (`It was a tie, can you believe it?`);
     }
     else if (playerScore > computerScore) {
-        console.log(`Player won the game.`)
+        gameTotalResult.textContent = (`Player won the game.`)
     }
     else {
-        console.log(`Computer won the game.`)
+        gameTotalResult.textContent = (`Computer won the game.`)
+    }
+    removeEvent();
+}
+
+function removeEvent () {
+    // Remove button events 
+      for (button of buttons) {
+        button.removeEventListener("click", game);
     }
 }
 
